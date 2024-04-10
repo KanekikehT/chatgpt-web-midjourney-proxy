8963e5660145a8f9528e06df63dc883165c1a7d9
Calvin <cai-zhiji@qq.com>
2024-04-10 09:16:06 +0800

    修改白天模式主题色

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -20,21 +20,26 @@ export function useTheme() {
   })
 
   const themeOverrides = computed<GlobalThemeOverrides>(() => {
-    if (!isDark.value) { // 当主题为明亮模式时
+    if (!isDark.value) {
+      // 白天模式样式
       return {
         common: {
           primaryColor: '#000000', // 主题色为黑色
-          primaryColorHover: '#000000', // 鼠标悬浮时为黑色
-          primaryColorPressed: '#000000', // 鼠标按下时为黑色
-          textColor: '#000000', // 文字颜色也设置为黑色，确保一致性
-
+          primaryColorHover: '#000000', // 鼠标悬浮时为浅灰色，提供微妙的反馈
+          primaryColorPressed: '#cccccc', // 鼠标按下时为中等灰色，增加交互性
+          textColor: '#000000', // 文字颜色为黑色，确保在白色背景上的可读性
+          borderColor: '#d9d9d9', // 边框颜色为浅灰色，维持整体的黑白风格同时确保边框可见性
+          bodyColor: '#ffffff', // 页面背景色为白色，清晰明亮
         },
-        // 根据需要，可以继续覆盖其他组件的主题变量
+        // 自定义其他组件样式...
       }
     }
-    else { // 暗色主题时使用默认或自定义的暗色主题配置
+    else {
+      // 黑夜模式样式
       return {
-        common: {},
+        common: {
+        },
+        // 自定义其他组件样式...
       }
     }
   })
