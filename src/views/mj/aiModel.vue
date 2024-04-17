1fbc6524ba6639041e88d26cc03477831699894f
Calvin <cai-zhiji@qq.com>
2024-04-18 00:49:46 +0800

    新增计算消耗文件，修改ai模型

--- src/views/mj/aiModel.vue
+++ src/views/mj/aiModel.vue
@@ -20,16 +20,17 @@ const nGptStore = ref(chatSet.getGptConfig())
 // })
 
 // gpt4.0 modal
+// const config = ref({
+//   model: ['gpt-4', 'gpt-4-0314', 'gpt-4-0613', 'gpt-4-1106-preview', 'gpt-4-0125-preview', 'gpt-4-vision-preview', 'gpt-4-1106-vision-preview', 'gpt-4-turbo', 'gpt-4-turbo-preview', 'gpt-4-turbo-2024-04-09', 'gpt-4-32k'],
+//   maxToken: 2048,
+// })
+
 const config = ref({
-  model: ['gpt-4', 'gpt-4-0314', 'gpt-4-0613', 'gpt-4-1106-preview', 'gpt-4-0125-preview', 'gpt-4-vision-preview', 'gpt-4-1106-vision-preview', 'gpt-4-turbo', 'gpt-4-turbo-preview', 'gpt-4-turbo-2024-04-09', 'gpt-4-32k'],
+  model: ['gpt-3.5-turbo', 'gpt-3.5-turbo-0125', 'gpt-4-all', 'gpt-4-32k'],
   maxToken: 2048,
 })
 
-// model:[ 'gpt-4-0125-preview','gpt-3.5-turbo',`gpt-4-1106-preview`,`gpt-3.5-turbo-16k`,'gpt-4','gpt-4-0613','gpt-4-32k-0613' ,'gpt-4-32k','gpt-4-32k-0314',`gpt-3.5-turbo-16k-0613`
-// ,`gpt-4-vision-preview`,`gpt-3.5-turbo-1106` ,'gpt-3.5-turbo-0125'
-// ,'gpt-3.5-turbo-0301','gpt-3.5-turbo-0613','gpt-4-all','gpt-3.5-net','gemini-pro'
-// ,'claude-3-sonnet-20240229','claude-3-opus-20240229','suno-v3'
-// ]
+// 'gpt-3.5-turbo', 'gpt-3.5-turbo-0125', 'gpt-4-all', 'gpt-4-32k'
 const st = ref({ openMore: false })
 const voiceList = computed(() => {
   const rz = []
